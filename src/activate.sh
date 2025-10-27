54c993839ba11d0a9a9cc6db73f68781cf90519b
bozenalabs <238575338+bozenalabs@users.noreply.github.com>
2025-10-27 22:17:36 +0000

    use bash preexec

--- src/activate.sh
+++ src/activate.sh
@@ -14,22 +14,10 @@ echo ""
 # bind '"q": "z\C-J"'
 # bind '"z": "exit"'
 
-# JOBU_SESSION_KEY="$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM"; # Random generates a number b/w 0 - 32767
-# JOBU_SESSION_KEY="${JOBU_SESSION_KEY}0000000000000000" # Pad it to 16+ chars.
-# export JOBU_SESSION_KEY=${JOBU_SESSION_KEY:0:16}; # Trim to 16-digits if excess.
-
 bind 'set enable-bracketed-paste off'
 
-PS1="MYPROMPT> "
-
 jobu_start_of_prompt() {
-    # printf "\033[?1049l"
-
-    # see test_8.sh for explanation
-    JOBU_ORIGINAL_PS0=$PS0
-    PS0="\033[?1049l"
-    # PS0=""
-    export PS0
+    PS1="MYPROMPT> "
     export PS1
     
     # Create a secure temporary file
@@ -40,31 +28,26 @@ jobu_start_of_prompt() {
     JOBU_COMMAND=$(<"$tmpfile")
     rm -f "$tmpfile"
 
-    # if [ $ret -ne 0 ]; then
-    #     echo "jobu get-command failed with exit code $ret" >&2
-    #     sleep 1
-    # fi
+    stty -echo
+    PS1=""
 
-    # If the command is empty or only whitespace, return early
-    # if [[ -z "$JOBU_COMMAND" || "${JOBU_COMMAND:0:1}" = "#" ]]; then
-    #     return
-    # fi
-    # echo "finished here"
     bind -x '"a": jobu_end_of_prompt'
     bind '"\e[0n": "a\C-J"'
     printf "\033[5n"
 }
 
-# Append to existing PROMPT_COMMAND instead of overwriting
-if [[ -n "$PROMPT_COMMAND" ]]; then
-    PROMPT_COMMAND="$PROMPT_COMMAND; jobu_start_of_prompt"
-else
-    PROMPT_COMMAND='jobu_start_of_prompt'
-fi
 
 jobu_end_of_prompt() {
-    echo "in end of prompt"
+    # echo "in end of prompt"
     READLINE_LINE=${JOBU_COMMAND};
     READLINE_POINT=${#READLINE_LINE};
-    printf "\033[?1049h"
 }
+
+jobu_pre_exec() {
+    stty echo
+}
+
+
+precmd_functions+=(jobu_start_of_prompt)
+preexec_functions+=(jobu_pre_exec)
+
--- test_rc.sh
+++ test_rc.sh
@@ -1,4 +1,6 @@
 
+source ./bash-preexec/bash-preexec.sh
+
 # eval "$(starship init bash)"
 
 source <(target/debug/jobu activate)
