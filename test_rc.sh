659e773855d9dc0b620bd45a9523d6717ce780f4
bozenalabs <238575338+bozenalabs@users.noreply.github.com>
2025-11-21 23:57:56 +0000

    got jobu to replace readline cleanly

--- test_rc.sh
+++ test_rc.sh
@@ -2,5 +2,6 @@ eval "$(starship init bash)"
 
 enable -f ./target/debug/libjobu.so jobu
 # can't set bash_input right now since bash_input is being used to read this file
-
-PROMPT_COMMAND="$PROMPT_COMMAND; jobu -s"
+jobu -s
+# echo "asdfsdfsdf"
+# PROMPT_COMMAND="$PROMPT_COMMAND; jobu -r"
