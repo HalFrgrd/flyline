3f63561f109ea185d6910a26285ca9f8042a746f
bozenalabs <238575338+bozenalabs@users.noreply.github.com>
2025-12-02 20:13:41 +0000

    gradual slow down in fps

--- src/events.rs
+++ src/events.rs
@@ -22,7 +22,8 @@ pub struct EventHandler {
 
 const ANIMATION_FPS_MAX: u64 = 60;
 const ANIMATION_FPS_MIN: u64 = 5;
-const ANIM_SWITCH_TIMEOUT_MS: u128 = 5000;
+const ANIM_SWITCH_INACTIVITY_START: u128 = 10000;
+const ANIM_SWITCH_INACTIVITY_LEN: u128 = 10000;
 
 impl EventHandler {
     pub fn new() -> Self {
@@ -46,16 +47,14 @@ impl EventHandler {
                     _ = tick_delay => {
                         sender_clone.send(Event::AnimationTick).unwrap();
 
-                        let period = if time_since_last_input.elapsed().as_millis() < ANIM_SWITCH_TIMEOUT_MS {
-                            Duration::from_millis(1000 / ANIMATION_FPS_MAX)
-                        } else {
-                            log::debug!("Switching to low FPS animation due to inactivity");
-                            Duration::from_millis(1000 / ANIMATION_FPS_MIN)
-                        };
-
+                        let inactivity_duration = time_since_last_input.elapsed().as_millis();
+                        let x: f32 = inactivity_duration.saturating_sub(ANIM_SWITCH_INACTIVITY_START) as f32 / ANIM_SWITCH_INACTIVITY_LEN as f32;
+                        let x = x.max(0.0).min(1.0);
+                        let fps = (ANIMATION_FPS_MAX as f32 * (1.0 - x)) + (ANIMATION_FPS_MIN as f32 * x);
+                        assert!(fps >= 0.0);
+                        // log::debug!("Inactivity duration: {}ms, setting animation FPS to {:.2}", inactivity_duration, fps);
+                        let period = Duration::from_millis((1000.0 / fps) as u64);
                         tick = tokio::time::interval_at((Instant::now() + period).into(), period);
-
-
                     }
                     Some(Ok(evt)) = crossterm_event =>{
                         match evt {
