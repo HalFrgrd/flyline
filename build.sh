3a6f45cd5a6dba7334ae12a092120d9ede01e981
Hal Frigaard <4559349+HalFrgrd@users.noreply.github.com>
2026-01-31 19:47:36 +0000

    github actions ci

--- build.sh
+++ build.sh
@@ -2,17 +2,12 @@
 set -Eeuo pipefail
 
 mkdir -p docker_build
-# container_name="flyline-tmp"
 
-# cleanup() {
-# 	docker rm -f "$container_name" >/dev/null 2>&1 || true
-# }
-# trap cleanup EXIT
+# docker build --target builder --tag flyline-test_builder --file Dockerfile .
+# # Run unit tests inside the builder image; use bash -lc to execute the command
+# docker run --rm flyline-test_builder bash -lc "cargo test --release --lib"
 
-# docker build --target flyline-builder-image --tag flyline-builder --file Dockerfile .
-# docker create --name "$container_name" flyline-builder
-# docker cp "$container_name":/app/target/release/libflyline.so ./docker_build/libflyline.so
-# # Container is removed by the trap on EXIT
 
-# this is a better way since we don't need to create and copy from a container
+# This doesnt build a container. The only file in the image's filesystem will be the built library
 DOCKER_BUILDKIT=1 docker build --file Dockerfile --target flyline_built_library -o docker_build/ .
+
