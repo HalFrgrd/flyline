9aae050ad22d487e9e8716cdaaaaf9032e9995c8
Hal Frigaard <4559349+HalFrgrd@users.noreply.github.com>
2026-03-03 22:45:23 +0000

    compiling

--- src/bash_symbols.rs
+++ src/bash_symbols.rs
@@ -203,7 +203,6 @@ unsafe extern "C" {
         compflags: c_int,
     ) -> *mut *mut c_char;
 
-
     // from readline/readline.h
     // Line buffer and maintenance
     // char *rl_line_buffer
@@ -274,10 +273,15 @@ unsafe extern "C" {
     pub static mut rl_completion_suppress_append: c_int;
 
     /* Character appended to completed words when at the end of the line.  The
-   default is a space. */
+    default is a space. */
     // int rl_completion_append_character = ' ';
     #[link_name = "rl_completion_append_character"]
     pub static mut rl_completion_append_character: c_int;
+
+    /* If non-zero, sort the completion matches.  On by default. */
+    // int rl_sort_completion_matches = 1;
+    #[link_name = "rl_sort_completion_matches"]
+    pub static mut rl_sort_completion_matches: c_int;
 
     // typedef char *rl_dequote_func_t (char *, int);
     // rl_dequote_func_t *rl_filename_dequoting_function
