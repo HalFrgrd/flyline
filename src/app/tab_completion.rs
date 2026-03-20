baff5a4c59bdbca070623dbaf64c6687a07bd7f3
HalFrgrd <4559349+HalFrgrd@users.noreply.github.com>
2026-03-20 09:44:38 +0100

    Fix demos (#310)

--- src/app/tab_completion.rs
+++ src/app/tab_completion.rs
@@ -293,6 +293,7 @@ impl App<'_> {
                 (quoted, suffix)
             };
             let ls_style = self.bash_env.style_for_path(path);
+            log::info!("For path '{}', determined ls style: {:?}", path.display(), ls_style);
             (appended.0, appended.1, ls_style)
         } else {
             (quoted, suffix, None)
