b86d53bc6569d098c4bdae8a3c7aac92b288b225
Hal Frigaard <4559349+HalFrgrd@users.noreply.github.com>
2026-03-22 15:03:10 +0000

    coloured custom anims

--- examples/animations.sh
+++ examples/animations.sh
@@ -1,3 +1,6 @@
+# Feel free to add your own animations here!
+
+
 flyline create-anim --name "MY_ANIMATION" --fps 10  ⣾ ⣷ ⣯ ⣟ ⡿ ⢿ ⣻ ⣽
 
 # Now you can use this animation in your prompt. For example:
@@ -14,3 +17,30 @@ flyline create-anim --name "john" --ping-pong --fps 8 \
     johN \
     john \
     john
+
+
+# Add styling with ANSI escape codes:
+BLACK='\e[30m'
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+BLUE='\e[34m'
+MAGENTA='\e[35m'
+CYAN='\e[36m'
+WHITE='\e[37m'
+
+flyline create-anim --name "john" --ping-pong --fps 3 \
+    "${YELLOW}john" \
+    "${RED}john" \
+    "${MAGENTA}john" \
+    "${BLUE}john" \
+    "${CYAN}john" \
+    "${GREEN}john" \
+    "${WHITE}john"
+
+
+# Experiment with higher frame rates for smooth transitions:
+
+# flyline create-anim --name "john" --ping-pong --fps 30 \
+#     '\e[38;2;255;0;0mjohn' \
+#     '\e[38;2;223;32;0mjohn' \
