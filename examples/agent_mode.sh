7a2890fdf7a4ea34f34b3417bd3ac76db0d4b7db
Hal Frigaard <4559349+HalFrgrd@users.noreply.github.com>
2026-04-01 21:54:20 +0100

    add codex example

--- examples/agent_mode.sh
+++ examples/agent_mode.sh
@@ -3,11 +3,15 @@ flyline agent-mode \
     --system-prompt "Be concise. Answer with a JSON array of at most 3 items with objects containing: command and description. Command will be a Bash command." \
     --command copilot --reasoning-effort low --prompt 
 
-# Claude
 # Claude has a --system-prompt flag so we could use that instead of making flyline prepend its system prompt, but for consistency with other agents we'll just prepend the system prompt in flyline.
 flyline agent-mode \
     --system-prompt "Be concise. Answer with a JSON array of at most 3 items with objects containing: command and description. Command will be a Bash command." \
     --command claude --effort low --prompt 
 
-# Others:
+# Codex:
+flyline agent-mode \
+    --system-prompt "Be concise. Answer with a JSON array of at most 3 items with objects containing: command and description. Command will be a Bash command." \
+    --command codex -a never exec -m 'GPT-5.1-Codex-Mini' --skip-git-repo-check --ephemeral --color always 
+
+
 # Feel free to add more agent examples!
