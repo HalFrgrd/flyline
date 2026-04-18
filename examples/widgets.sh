1cef0b5c00573157269fb060233012a7d1fd083c
HalFrgrd <4559349+HalFrgrd@users.noreply.github.com>
2026-04-18 16:07:33 +0100

    Update widgets.sh

--- examples/widgets.sh
+++ examples/widgets.sh
@@ -1,4 +1,3 @@
-
 flyline create-prompt-widget custom --name GIT_METRICS --command "starship module git_metrics" --placeholder prev
 
 flyline create-prompt-widget mouse-mode --name MOUSE_MODE "on " "off"
