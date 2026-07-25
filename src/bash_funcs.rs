13eb9eb974fd0a2e679a6e0ddf98ead6f9d851fb
Hal Frigaard <4559349+HalFrgrd@users.noreply.github.com>
2026-07-25 14:38:09 +0100

    fmt

--- src/bash_funcs.rs
+++ src/bash_funcs.rs
@@ -1775,7 +1775,7 @@ impl ExecutablesOnPath {
 
     /// Update the cache in-place: evict removed PATH dirs, add new ones, and
     /// re-scan any directory whose mtime has changed.
-    /// 
+    ///
     /// This is pure file system stuff and should never require BASH_LOCK.
     fn update_cache(path_env: Option<String>) {
         let _timer = crate::perf::PerfTimer::start_and_log_on_drop("update_path_cache");
