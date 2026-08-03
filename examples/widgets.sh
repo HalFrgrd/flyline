733b89a7ee8524f5c92a6680a39284a68d8f17af
Hal Frigaard <4559349+HalFrgrd@users.noreply.github.com>
2026-08-03 22:12:56 +0100

    starhsip issue

--- examples/widgets.sh
+++ examples/widgets.sh
@@ -1,5 +1,9 @@
-flyline create-prompt-widget custom --name GIT_METRICS --command "starship module git_metrics" --placeholder prev
-
+# Flyline widgets:
 flyline create-prompt-widget mouse-mode --name MOUSE_MODE "on " "off"
 flyline create-prompt-widget copy-buffer '[copy]'
 flyline create-prompt-widget last-command-duration
+
+# Starship widgets:
+# NB: for these to work, they must be enabled in starship.toml config
+flyline create-prompt-widget custom --name GIT_METRICS --command "starship module git_metrics" --placeholder prev
+
